cc279801a1d9337d810fc34195c34572f71fe80b
raahi-chada2004 <raahi.chada@gmail.com>
2024-07-10 20:50:05 +0000

    working end-to-end integration test

---
+++ integration_test.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"os/exec"
+	"testing"
+	"text/template"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/api/iterator"
+)
+
+// integration test validates the end-to-end fluentbit and bigquery pipeline
+func TestPipeline(t *testing.T) {
+	logFilePath := "./logfile.log"
+	configFilePath := "./fluent-bit.conf"
+	ctx := context.Background()
+
+	// Set up BigQuery client
+	client, err := bigquery.NewClient(ctx, "bigquerytestdefault")
+	if err != nil {
+		t.Fatalf("Failed to create BigQuery client: %v", err)
+	}
+	defer client.Close()
+
+	// Create a random table name
+	hash := randString()
+	tableID := "test_" + hash
+
+	// Create BigQuery table in an existing dataset and project
+	datasetID := "siddag_summer2024"
+	tableSchema := bigquery.Schema{
+		{Name: "Message", Type: bigquery.StringFieldType},
+	}
+	dataset := client.Dataset(datasetID)
+	table := dataset.Table(tableID)
+	if err := table.Create(ctx, &bigquery.TableMetadata{Schema: tableSchema}); err != nil {
+		t.Fatalf("Failed to create BigQuery table: %v", err)
+	}
+
+	//Create config file with random table name
+	if err := createConfigFile(tableID); err != nil {
+		t.Fatalf("failed to create config file: %v", err)
+	}
+
+	// Create log file
+	file, err := os.Create(logFilePath)
+	if err != nil {
+		t.Fatalf("Failed to create log file: %v", err)
+	}
+	defer file.Close()
+
+	// Start Fluent Bit with the config file
+	FBcmd := exec.Command("fluent-bit", "-c", "fluent-bit.conf")
+	if err := FBcmd.Start(); err != nil {
+		t.Fatalf("Failed to start Fluent Bit: %v", err)
+	}
+
+	//Wait for fluent-bit connection to generate data; add delays before ending fluent-bit process
+	time.Sleep(2 * time.Second)
+	if err := generateData(10); err != nil {
+		t.Fatalf("Failed to generate data: %v", err)
+	}
+	time.Sleep(2 * time.Second)
+
+	// Stop Fluent Bit
+	if err := FBcmd.Process.Kill(); err != nil {
+		t.Fatalf("Failed to stop Fluent Bit: %v", err)
+	}
+
+	// Verify data in BigQuery by querying
+	queryMsg := "SELECT Message FROM `bigquerytestdefault.siddag_summer2024." + tableID + "`"
+	BQquery := client.Query(queryMsg)
+	BQdata, err := BQquery.Read(ctx)
+	if err != nil {
+		t.Fatalf("Failed to query data information BigQuery: %v", err)
+	}
+
+	rowCount := 0
+	for {
+		//Check that the data sent is correct
+		var BQvalues []bigquery.Value
+		err := BQdata.Next(&BQvalues)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			t.Fatalf("Failed to read query results: %v", err)
+		}
+
+		assert.Equal(t, "hello world", BQvalues[0])
+		rowCount++
+	}
+
+	// Verify the number of rows
+	assert.Equal(t, 10, rowCount)
+
+	// Clean up - delete the BigQuery table
+	if err := table.Delete(ctx); err != nil {
+		t.Fatalf("Failed to delete BigQuery table: %v", err)
+	}
+
+	// Clean up - delete the log file
+	if err := os.Remove(logFilePath); err != nil {
+		t.Fatalf("Failed to delete log file: %v", err)
+	}
+
+	// Clean up - delete the config file
+	if err := os.Remove(configFilePath); err != nil {
+		t.Fatalf("Failed to delete log file: %v", err)
+	}
+}
+
+// generate a random string with length 10 (to act as a hash for the table name)
+func randString() string {
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	charset := "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
+	str := make([]byte, 10)
+	for i := range str {
+		str[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(str)
+}
+
+// template for the config file - dynamically update TableId field
+const configTemplate = `
+[SERVICE]
+    Daemon          off
+    Log_Level       error
+    Parsers_File    ./jsonparser.conf
+    plugins_file    ./plugins.conf
+[INPUT]
+    Name                               tail
+    Path                               ./logfile.log
+    Parser                             json
+    Tag                                hello_world
+[OUTPUT]
+    Name                               writeapi
+    Match                              hello_world
+    ProjectId                          bigquerytestdefault
+    DatasetId                          siddag_summer2024
+    TableId                            {{.CurrTableName}}
+`
+
+// struct for dynamically updating config file
+type Config struct {
+	CurrTableName string
+}
+
+// function creates configuration file with the input as the TableId field
+func createConfigFile(currTableID string) error {
+	//struct with the TableId
+	config := Config{CurrTableName: currTableID}
+
+	//create a new template with the format of configTemplate
+	tmpl, err := template.New("currConfig").Parse(configTemplate)
+	if err != nil {
+		return err
+	}
+
+	//create the new file
+	file, err := os.Create("./fluent-bit.conf")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//return file with the given template and TableId
+	return tmpl.Execute(file, config)
+}
+
+//Log entry template (corresponding to BQ table schema)
+
+type log_entry struct {
+	Message string `json:"Message"`
+}
+
+// data generation function
+func generateData(numRows int) error {
+	//open file
+	file, err := os.OpenFile("logfile.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	logger := log.New(file, "", 0)
+	if err != nil {
+		return err
+	}
+
+	//send json marshalled data
+	for i := 0; i < numRows; i++ {
+		curr := log_entry{
+			Message: "hello world",
+		}
+		entry, err := json.Marshal(curr)
+		if err != nil {
+			return err
+		}
+		logger.Println(string(entry))
+	}
+
+	return nil
+}
