db26a38520c92c2391d977a1aa7c9c406fc3e5d9
raahi-chada2004 <raahi.chada@gmail.com>
2024-07-11 23:10:28 +0000

    code review

--- out_writeapi_test.go
+++ out_writeapi_test.go
@@ -1,11 +1,29 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package main
 
 import (
+	"context"
+	"log"
 	"testing"
 	"unsafe"
 
 	"bou.ke/monkey"
+	"cloud.google.com/go/bigquery/storage/apiv1/storagepb"
+	"cloud.google.com/go/bigquery/storage/managedwriter"
 	"github.com/fluent/fluent-bit-go/output"
+	"github.com/googleapis/gax-go/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,5 +54,152 @@ func TestFLBPluginRegister(t *testing.T) {
 
 	assert.Equal(t, 0, result)
 	assert.Equal(t, "writeapi", currplugin.name)
+
+}
+
+// this is a struct keeping track of whether the correct options are sent in NewManagedStream
+type OptionChecks struct {
+	configProjectID        bool
+	configDatasetID        bool
+	configTableID          bool
+	configMaxChunkSize     bool
+	configMaxQueueSize     bool
+	configMaxQueueRequests bool
+	calledGetClient        int
+	calledNewManagedStream int
+	calledGetWriteStream   int
+	calledSetContext       int
+	numInputs              bool
+	mapSizeIncremented     bool
+}
+
+type MockManagedWriterClient struct {
+	client                      *managedwriter.Client
+	NewManagedStreamFunc        func(ctx context.Context, opts ...managedwriter.WriterOption) (*managedwriter.ManagedStream, error)
+	GetWriteStreamFunc          func(ctx context.Context, req *storagepb.GetWriteStreamRequest, opts ...gax.CallOption) (*storagepb.WriteStream, error)
+	CloseFunc                   func() error
+	BatchCommitWriteStreamsFunc func(ctx context.Context, req *storagepb.BatchCommitWriteStreamsRequest, opts ...gax.CallOption) (*storagepb.BatchCommitWriteStreamsResponse, error)
+	CreateWriteStreamFunc       func(ctx context.Context, req *storagepb.CreateWriteStreamRequest, opts ...gax.CallOption) (*storagepb.WriteStream, error)
+}
+
+func (m *MockManagedWriterClient) NewManagedStream(ctx context.Context, opts ...managedwriter.WriterOption) (*managedwriter.ManagedStream, error) {
+	return m.NewManagedStreamFunc(ctx, opts...)
+}
+
+func (m *MockManagedWriterClient) GetWriteStream(ctx context.Context, req *storagepb.GetWriteStreamRequest, opts ...gax.CallOption) (*storagepb.WriteStream, error) {
+	return m.GetWriteStreamFunc(ctx, req, opts...)
+}
+
+func (m *MockManagedWriterClient) Close() error {
+	return m.client.Close()
+}
+
+func (m *MockManagedWriterClient) BatchCommitWriteStreams(ctx context.Context, req *storagepb.BatchCommitWriteStreamsRequest, opts ...gax.CallOption) (*storagepb.BatchCommitWriteStreamsResponse, error) {
+	return m.client.BatchCommitWriteStreams(ctx, req, opts...)
+}
+
+func (m *MockManagedWriterClient) CreateWriteStream(ctx context.Context, req *storagepb.CreateWriteStreamRequest, opts ...gax.CallOption) (*storagepb.WriteStream, error) {
+	return m.client.CreateWriteStream(ctx, req, opts...)
+}
+
+// TestFLBPluginInit tests the FLBPluginInit function
+func TestFLBPluginInit(t *testing.T) {
+	//variable keeping track of what order functions are called in
+	order := 0
+	var currChecks OptionChecks
+	mockClient := &MockManagedWriterClient{
+		NewManagedStreamFunc: func(ctx context.Context, opts ...managedwriter.WriterOption) (*managedwriter.ManagedStream, error) {
+			order++
+			//checking to see if function was called third
+			if order == 3 {
+				currChecks.calledNewManagedStream++
+			}
+			if len(opts) == 6 {
+				currChecks.numInputs = true
+			}
+			return nil, nil
+
+		},
+		GetWriteStreamFunc: func(ctx context.Context, req *storagepb.GetWriteStreamRequest, opts ...gax.CallOption) (*storagepb.WriteStream, error) {
+			order++
+			//checking to see if function was called second
+			if order == 2 {
+				currChecks.calledGetWriteStream++
+			}
+			return &storagepb.WriteStream{
+				Name: "mockstream",
+				TableSchema: &storagepb.TableSchema{
+					Fields: []*storagepb.TableFieldSchema{
+						{Name: "Time", Type: storagepb.TableFieldSchema_STRING, Mode: storagepb.TableFieldSchema_NULLABLE},
+						{Name: "Text", Type: storagepb.TableFieldSchema_STRING, Mode: storagepb.TableFieldSchema_NULLABLE},
+					},
+				},
+			}, nil
+		},
+	}
+
+	originalFunc := getClient
+	getClient = func(ctx context.Context, projectID string) (ManagedWriterClient, error) {
+		order++
+		//checking to see if function was called first
+		if order == 1 {
+			currChecks.calledGetClient++
+		}
+		return mockClient, nil
+	}
+	defer func() { getClient = originalFunc }()
+
+	patch1 := monkey.Patch(output.FLBPluginConfigKey, func(plugin unsafe.Pointer, key string) string {
+		log.Println("Mock out.FLBPluginConfigKey called")
+		switch key {
+		case "ProjectID":
+			currChecks.configProjectID = true
+			return "DummyProjectId"
+		case "DatasetID":
+			currChecks.configDatasetID = true
+			return "DummyDatasetId"
+		case "TableID":
+			currChecks.configTableID = true
+			return "DummyTableId"
+		case "Max_Chunk_Size":
+			currChecks.configMaxChunkSize = true
+			return "0"
+		case "Max_Queue_Requests":
+			currChecks.configMaxQueueRequests = true
+			return "0"
+		case "Max_Queue_Bytes":
+			currChecks.configMaxQueueSize = true
+			return "0"
+		default:
+			return ""
+		}
+	})
+	defer patch1.Unpatch()
+
+	patch2 := monkey.Patch(output.FLBPluginSetContext, func(plugin unsafe.Pointer, ctx interface{}) {
+		currChecks.calledSetContext++
+	})
+	defer patch2.Unpatch()
+
+	plugin := unsafe.Pointer(nil)
+	initsize := len(configMap)
+	result := FLBPluginInit(plugin)
+	finsize := len(configMap)
+	if (finsize - 1) == initsize {
+		currChecks.mapSizeIncremented = true
+	}
+	assert.Equal(t, output.FLB_OK, result)
+	assert.True(t, currChecks.configProjectID)
+	assert.True(t, currChecks.configDatasetID)
+	assert.True(t, currChecks.configTableID)
+	assert.True(t, currChecks.configMaxChunkSize)
+	assert.True(t, currChecks.configMaxQueueRequests)
+	assert.True(t, currChecks.configMaxQueueSize)
+	assert.Equal(t, 1, currChecks.calledGetClient)
+	assert.Equal(t, 1, currChecks.calledGetWriteStream)
+	assert.Equal(t, 1, currChecks.calledNewManagedStream)
+	assert.Equal(t, 1, currChecks.calledSetContext)
+	assert.True(t, currChecks.numInputs)
+	assert.True(t, currChecks.mapSizeIncremented)
 
 }
